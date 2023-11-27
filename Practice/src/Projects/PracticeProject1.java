f01b93d9f777b782eba0b44b8e342d66219c0d77
Dhanush <dhanush@company.com>
2023-11-27 18:46:27 +0530

    Project1

---
+++ Practice/src/Projects/PracticeProject1.java
@@ -0,0 +1,15 @@
+package Projects;
+
+public class PracticeProject1 {
+
+	public static void main(String[] args) {
+		int a=7;
+		float f= a; //Implicit Type Casting
+		System.out.println(f);
+		
+		float b=18.7f;
+		int d=  (int) b;
+		System.out.println(b);
+	}
+
+}
